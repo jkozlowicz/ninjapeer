20eff806c579863d0f0b36fcab7f6fa679ff9628
jkozlowicz <jakub.kozlowicz@gmail.com>
2014-09-27 00:51:09 +0200

    Working on frontend

--- static/js/main.js
+++ static/js/main.js
@@ -17,12 +17,39 @@ $(document).ready(function(){
         event.preventDefault();
     });
 
+    $(".download-file").click(onClickDownloadFile);
+
 });
+
+var onClickDownloadFile = function(event){
+    var currRow = $(this).parent().parent();
+    var fileName = currRow.find('.result-name').textContent;
+    var data = {
+        "action": "DOWNLOAD",
+        "value": fileName
+    };
+    var json_str = JSON.stringify(data);
+    websocket.send(json_str);
+};
 
 var hideLoadingAnimation = function(){
     $('#loading-img-container').addClass("hidden");
-}
+};
 
 var showLoadingAnimation = function(){
     $('#loading-img-container').removeClass("hidden");
-}
+};
+
+var addResult = function(name, date, size){
+    var row = '<tr class="result-item">' +
+	'<td class="vert-align result-name">' + name + '</td>' +
+	'<td class="vert-align result-date">' + date + '</td>' +
+	'<td class="vert-align result-size">' + size + '</td>' +
+    '<td class="vert-align"><a href="#"><span class="glyphicon glyphicon-download glyphicon-big"></span></a></td>' +
+	'</tr>';
+};
+
+//var bar = $('div .progress-bar-striped')[0];
+//
+//$(bar).removeClass('active');
+//$(bar).removeClass('progress-bar-striped');
